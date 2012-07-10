87559ab2375ee1ce54cc40e31d1ca53b300b62d9
Casey Li <casey.li@gmail.com>
2012-07-10 13:35:41 -0400

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrhing::Application.config.secret_token = '0286e01c17202f145ba125daf42c102049c8d8e5e7fa0d607eb78bfd2a6d543459be7f31e333168e5b5c3f75fee9c93b038a1ed04a2b797ca42bdc8c77816735'
