4c3821977627a9c66926a71c47efc2899120dbc1
Casey Li <casey.li@gmail.com>
2012-07-10 13:54:38 -0400

    Basic Devise Authentication Implemented.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Mrhing::Application.routes.draw do
+  
+  devise_for :users
+
+  get "pages/home"
+
+  root :to => "pages#home"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
