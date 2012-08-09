0f5ca925a31b4194e599364c96780ca1302f0530
Casey Li <casey.li@gmail.com>
2012-08-09 01:53:54 -0400

    Fixed scoping of ancestry arrange.

--- app/models/discussion_post.rb
+++ app/models/discussion_post.rb
@@ -1,7 +1,6 @@
 class DiscussionPost < ActiveRecord::Base
-  default_scope order("updated_at desc")
   attr_accessible :body, :posted_by, :topic_id, :parent_id
   belongs_to :topic
   
-  has_ancestry
+  has_ancestry cache_depth: true
 end
